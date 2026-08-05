b5a86020f14ffe469b33c94bd10c1772c8a40981
17carlosdaniel <carlosdanielribeiro989@gmail.com>
2026-08-05 20:28:35 -0300

    atualizaçao

--- desafio.py
+++ desafio.py
@@ -1,6 +1,26 @@
 nome = "Felipao"
 xp =  1715
 
-if 1000 <= xp <= 2000:
-    print(f"O Heroi de nome {nome}esta no nivel {xp}")
- 
+nome = "Felipao"
+xp = 1715
+
+# Classificação dos níveis sem deixar buracos entre os números
+if xp <= 1000:
+    nivel = "Ferro"
+elif 1001 <= xp <= 2000:
+    nivel = "Bronze"
+elif 2001 <= xp <= 5000:
+    nivel = "Prata"
+elif 5001 <= xp <= 7000:
+    nivel = "Ouro"
+elif 7001 <= xp <= 8000:
+    nivel = "Platina"
+elif 8001 <= xp <= 9000:
+    nivel = "Ascendente"
+elif 9001 <= xp <= 10000:
+    nivel = "Imortal"
+else:
+    nivel = "Radiante"
+
+# Um único print no final resolve tudo sem repetir código!
+print(f"O Herói de nome {nome} está no nível de {nivel}")
