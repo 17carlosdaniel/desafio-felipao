be78a35d135898b9273364538e78afe5a74d4423
17carlosdaniel <carlosdanielribeiro989@gmail.com>
2026-08-11 13:03:34 -0300

    Desafio de calculadora de partidas ranqueadas concluído

---
+++ main.js
@@ -0,0 +1,35 @@
+let vitorias = 70
+let derrotas = 20
+
+function calcularRank(vitorias, derrotas) {
+
+    let saldoVitorias = vitorias - derrotas;
+    let nivel;
+
+    if (saldoVitorias <= 10) {
+        nivel = "Ferro";
+    } else if (saldoVitorias <= 20) {
+        nivel = "Bronze";
+    } else if (saldoVitorias <= 50) {
+        nivel = "Prata";
+    } else if (saldoVitorias <= 80) {
+        nivel = "Ouro";
+    } else if (saldoVitorias <= 90) {
+        nivel = "Diamante";
+    } else if (saldoVitorias <= 100) {
+        nivel = "Lendário";
+    } else {
+        nivel = "Imortal";
+    }
+
+    return {
+        saldoVitorias: saldoVitorias,
+        nivel: nivel
+    };
+}
+
+let resultado = calcularRank(vitorias, derrotas);
+
+console.log(
+    `O Herói tem saldo de ${resultado.saldoVitorias} e está no nível de ${resultado.nivel}`
+);
