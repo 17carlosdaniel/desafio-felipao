dacd28b5742f67c471f032aadf1d5d68149896cb
17carlosdaniel <carlosdanielribeiro989@gmail.com>
2026-08-18 16:13:24 -0300

    up

--- main.js
+++ main.js
@@ -1,23 +1,29 @@
-let vitorias = 70
-let derrotas = 20
+let vitorias = 70;
+let derrotas = 20;
 
 function calcularRank(vitorias, derrotas) {
 
     let saldoVitorias = vitorias - derrotas;
     let nivel;
 
-    if (saldoVitorias <= 10) {
+    if (vitorias <= 10) {
         nivel = "Ferro";
-    } else if (saldoVitorias <= 20) {
+
+    } else if (vitorias <= 20) {
         nivel = "Bronze";
-    } else if (saldoVitorias <= 50) {
+
+    } else if (vitorias <= 50) {
         nivel = "Prata";
-    } else if (saldoVitorias <= 80) {
+
+    } else if (vitorias <= 80) {
         nivel = "Ouro";
-    } else if (saldoVitorias <= 90) {
+
+    } else if (vitorias <= 90) {
         nivel = "Diamante";
-    } else if (saldoVitorias <= 100) {
+
+    } else if (vitorias <= 100) {
         nivel = "Lendário";
+
     } else {
         nivel = "Imortal";
     }
